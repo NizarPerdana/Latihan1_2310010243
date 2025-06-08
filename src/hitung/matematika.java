eac6a56bf2aa459bec57dd58e4b187a25db77a3c
NizarPerdana <nizarperdanam@gmail.com>
2025-06-08 21:01:59 +0800

    Melengkapi method matematika

--- src/hitung/matematika.java
+++ src/hitung/matematika.java
@@ -1,13 +1,34 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package hitung;
 
-/**
- *
- * @author ASUS
- */
+
 public class matematika {
+    //membuat variable
+    private double bil1, bil2;
     
+    //membuat construktor
+    public matematika(double bil1, double bil2){
+        this.bil1 = bil1;
+        this.bil2 = bil2;
+    }
+    
+    //membuat method set penjumlahan
+    public double setPenjumlahan(){
+        return bil1 + bil2;
+    }
+    
+    //membuat method setpengurangan
+    public double setPengurangan(){
+        return bil1 - bil2;
+    }
+    
+    //membuat method setperkalian
+    public double setPerkalian(){
+        return bil1 * bil2;
+    }
+    
+    //membuat method setpembagian
+    public double setPembagian(){
+        return bil1 / bil2;
+    }
 }
